1228fd6ac51ab7eb5c0b9ff52f3d11586ef2f621
jack-nikky <429416580@qq.com>
2020-10-06 10:21:04 +0800

    springboot使用Thymeleaf

--- SpringBootProject1/itheima_springboot/src/main/java/com/itheima/controller/UserController.java
+++ SpringBootProject1/itheima_springboot/src/main/java/com/itheima/controller/UserController.java
@@ -4,24 +4,30 @@ import com.itheima.pojo.User;
 import com.itheima.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@RestController
+//@RestController
+@Controller
 @MapperScan("com.itheima.mapper")
 public class UserController {
     @Autowired
     private UserService userService;
 
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
 
     @GetMapping("/hello")
+    @ResponseBody
     public String hello(){
         return "hello, my springboot project";
     }
 
     @GetMapping("/user/{id}")
+    @ResponseBody
     public User queryById(@PathVariable Long id)  {
         return userService.queryById(id);
     }
